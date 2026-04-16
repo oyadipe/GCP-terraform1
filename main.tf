cc9affb017e0e9eeab00f38e9dba37a208f02ac7
oyadipe <oluwagbenga.y@gmail.com>
2026-04-16 19:25:21 -0400

    i need to change the name of the bucket

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my_bucket" {
   project = "ancient-chiller-433403-k9"
-  name     = "my-unique-bucket-name"
+  name     = "19031982-terraform-bucket"
   location = "US"
   force_destroy = true
   
