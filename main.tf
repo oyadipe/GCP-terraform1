e4cdc9ee4d9e85902f7f2882eea8dfe4b11dcf51
oyadipe <oluwagbenga.y@gmail.com>
2026-04-17 17:03:13 -0400

    still testing

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "google_storage_bucket" "my_bucket" {
-  project = "ancient-chiller-433403-k9"
-  name     = "19031982-terraform-bucket"
-  location= "us-central1"
-  force_destroy = true
+  project                  = "ancient-chiller-433403-k9"
+  name                     = "19031982-terraform-bucket"
+  location                 = "us-central1"
+  force_destroy            = true
   public_access_prevention = false
-  
+
 }
