d0024496326b4f52937c0e63f698fc8a0ebefcae
oyadipe <oluwagbenga.y@gmail.com>
2026-04-17 17:24:39 -0400

    i needed to provider block to main.tf file

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project = "replace with your project id"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
 resource "google_storage_bucket" "my_bucket" {
   project                  = "ancient-chiller-433403-k9"
   name                     = "19031982-terraform-bucket"
