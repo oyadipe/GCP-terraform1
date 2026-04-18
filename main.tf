77b7d16ace11c97ff5c3181608b5fc0d920c84c2
oyadipe <oluwagbenga.y@gmail.com>
2026-04-17 23:07:54 -0400

    several adjustment to ymal file for github workflow

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
-provider "google" {
-  project = "replace with your project id"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.28.0"
+    }
+  }
 }
+
+provider "google" {
+  project = "ancient-chiller-433403-k9"
+}
+
 resource "google_storage_bucket" "my_bucket" {
   project                  = "ancient-chiller-433403-k9"
   name                     = "19031982-terraform-bucket"
   location                 = "us-central1"
   force_destroy            = true
   public_access_prevention = false
-
 }
